47f276428fc1e2694119baedefeafeb95e970d5c
lcds2019 <lcds.gru@gmail.com>
2023-08-01 18:53:20 -0300

    Capítulo 02 - Curso relâmpago de Python - Strings & exceptions

---
+++ cap02B_curso_de_python.py
@@ -0,0 +1,31 @@
+print('')
+
+########################################################################
+print(f'Strings -->> '.ljust(80 ,'#'))
+
+texto = 'Exemplo de string com aspas simples'
+print(texto)
+
+print('')
+texto2 = r'\\' #exemplo de texto bruto com a função 'r'
+print(f'Exemplo da aplicação da função r para textos brutos: {texto2})')
+
+print('')
+print(f'Exemplo de textos em múltiplsa linhas:')
+multilinestring='''
+           linha um 
+           linha dois
+           linha três '''
+print(multilinestring)
+
+########################################################################
+print(f'Exceções -->> '.ljust(80 ,'#'))
+
+lista = range(0,10,2)
+
+for i in lista:
+    print(i)
+    try:
+        print(i/0)
+    except:
+        print('Error by division!!')
